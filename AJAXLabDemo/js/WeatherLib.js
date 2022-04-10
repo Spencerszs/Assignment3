3fcbdd8dbc3873f3d55749cbe8b8ced95c82d410
Spencerszs <99428040+Spencerszs@users.noreply.github.com>
2022-04-10 16:28:52 -0700

    Add files via upload

---
+++ AJAXLabDemo/js/WeatherLib.js
@@ -0,0 +1,138 @@
+let stationsList = [];
+
+function processXML(xmlDoc) {
+    let stationsXML = xmlDoc.getElementsByTagName("station");
+
+    for (x = 0; x < stationsXML.length; x++) {
+
+        // Create the parallel object list.
+        let station_idXML = stationsXML[x].getElementsByTagName("station_id")[0].innerHTML;
+        let stateXML = stationsXML[x].getElementsByTagName("state")[0].innerHTML;
+        let station_nameXML = stationsXML[x].getElementsByTagName("station_name")[0].innerHTML;
+        let latitudeXML = stationsXML[x].getElementsByTagName("latitude")[0].innerHTML;
+        let longitudeXML = stationsXML[x].getElementsByTagName("longitude")[0].innerHTML;
+        let xml_urlXML = stationsXML[x].getElementsByTagName("xml_url")[0].innerHTML;
+
+        let station = {
+            stationid: station_idXML,
+            state: stateXML,
+            stationName: station_nameXML,
+            latitude: latitudeXML,
+            longitude: longitudeXML,
+            xmlURL: xml_urlXML
+        };
+        stationsList.push(station);
+    }
+}
+
+function getStations(state) {
+    let returnList = null;
+    returnList = stationsList.filter(
+        function (o) { return o.state == state });
+    return (returnList);
+}
+
+let _hiddenState = null;
+
+function getStations2(state) {
+    let returnList = null;
+    _hiddenState = state;
+    returnList = stationsList.filter(isSameState);
+    return (returnList);
+}
+
+function isSameState(obj)
+{
+    let match = obj.state == _hiddenState
+    return (match);
+}
+
+function getStation(stationid) {
+    let returnStation = null;
+    returnStation = stationList.find(function (o) { return o.stationid = stationid });
+    return (returnStation);
+}
+
+function displayStationsList(stationsList, divAttach) {
+    let uList = document.createElement("ul");
+    for (x = 0; x < stationsList.length; x++) {
+
+        let stationid = stationsList[x].stationid;
+        let state = stationsList[x].state;
+        let station_name = stationsList[x].stationName;
+        let listItem = stationid + " (" + state + ") - " + station_name;
+
+        let liCurrent = document.createElement("li");
+        liCurrent.innerHTML = listItem;
+        uList.appendChild(liCurrent);
+    }
+    removeAllChildren(divAttach);
+    divAttach.appendChild(uList);
+}
+
+function createTableHeader(tblArg) {
+    let hdrRow = tblArg.insertRow();
+    let cellStationId = hdrRow.insertCell();
+    cellStationId.appendChild(document.createTextNode("Station Id"));
+    hdrRow.appendChild(cellStationId)
+
+    let cellStationName = hdrRow.insertCell();
+    cellStationName.appendChild(document.createTextNode("Station Name"));
+    hdrRow.appendChild(cellStationName);
+
+    let cellStationLat = hdrRow.insertCell();
+    cellStationLat.appendChild(document.createTextNode("Latitude"));
+    hdrRow.appendChild(cellStationLat);
+
+    let cellStationLong = hdrRow.insertCell();
+    cellStationLong.appendChild(document.createTextNode("Longitude"));
+    hdrRow.appendChild(cellStationLong);
+
+    let cellStationState = hdrRow.insertCell();
+    cellStationState.appendChild(document.createTextNode("State"));
+    hdrRow.appendChild(cellStationState);
+}
+
+function createTableRow(tblArg, stationIdArg, stationNameArg, stationStateArg, lat, long ) {
+    let curRow = tblArg.insertRow();
+
+    let cellStationId = curRow.insertCell();
+    cellStationId.appendChild(document.createTextNode(stationIdArg));
+    curRow.appendChild(cellStationId);
+
+    let cellStationName = curRow.insertCell();
+    cellStationName.appendChild(document.createTextNode(stationNameArg));
+    curRow.appendChild(cellStationName);
+
+
+    let cellStationLat = curRow.insertCell();
+    cellStationLat.appendChild(document.createTextNode(lat));
+    curRow.appendChild(cellStationLat);
+
+    let cellStationLong = curRow.insertCell();
+    cellStationLong.appendChild(document.createTextNode(long));
+    curRow.appendChild(cellStationLong);
+
+    let cellStationState = curRow.insertCell();
+    cellStationState.appendChild(document.createTextNode(stationStateArg));
+    curRow.appendChild(cellStationState);
+}
+
+function displayStationTable(stationsList, divAttach) {
+    
+    divAttach.innerHTML = ""; // remove previous search
+    let uTable = document.createElement("table");
+    createTableHeader(uTable);
+    divAttach.appendChild(uTable);
+
+    for (x = 0; x < stationsList.length; x++) {
+        createTableRow(uTable,
+            stationsList[x].stationid,
+            stationsList[x].stationName,
+            stationsList[x].state,
+            stationsList[x].latitude,
+            stationsList[x].longitude);
+    }
+}
+
+// 50 60 70 80 90 100
